adc6cd5380fdb58affc2d68c829a416597ea1ce5
Rajat Rajput <rajat.r1024@gmail.com>
2026-08-21 17:30:54 +0530

    Admin done and backend updated

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 
-    mongoose.connection.on('connected', () => {
-        console.log('DB connected');
+    mongoose.connection.on('connected',() => {
+        console.log("DB Connected");
     })
 
-        await mongoose.connect(`${process.env.MONGODB_URI}/ecom`)
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`)
 
 }
 
--- backend/models/productModel.js
+++ backend/models/productModel.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
 
 const productSchema = new mongoose.Schema({
-    name: {type: String, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    image: {type: Array, required: true},
-    category: {type: String, required: true},
-    subCategory: {type: String, required: true},
-    sizes: {type: [String], required: true},
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: Array, required: true },
+    category: { type: String, required: true },
+    subCategory: { type: String, required: true },
+    sizes: { type: Array, required: true },
     // sizes: {type: Array, required: true},
     // You can also use the following line instead of the above line if you want to store sizes as an array of strings
-    bestSeller: {type: Boolean},
-    date: {type: Number, required: true}
+    bestseller: { type: Boolean },
+    date: { type: Number, required: true }
 })
 
-const productModel = mongoose.models.product || mongoose.model("product", productSchema);
+const productModel  = mongoose.models.product || mongoose.model("product",productSchema);
 
-export default productModel;
+export default productModel
